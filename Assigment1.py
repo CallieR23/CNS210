045e3e9400ef043d1bbe24414fb708973fbba1ba
CallieR23 <62801357+CallieR23@users.noreply.github.com>
2020-04-11 14:32:05 -0400

    Update Assigment1.py

--- Assigment1.py
+++ Assigment1.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python37
 import os.path
 import argparse
-
-parser = argparse.ArgumentParser(description='This program finds the nth number in the Fibonacci sequence and outputs to the file directed.')
-
-parser.add_argument('-n', metavar='number', dest='number', type=int, nargs="+", help="Sequence number in Fibonacci sequence")
-parser.add_argument('-f', metavar='filename', type=str, nargs="+", help="String for file name")
-
-args = parser.parse_args()
-number = args.number
-print(args.number)
-
-filename=input("What is the name of the file: ")
-number=input("How many terms: ")
-
 def fibonacci(number):
     if number == 0:
         return 0
@@ -22,16 +9,35 @@ def fibonacci(number):
     else:
         return fibonacci(number-1) + fibonacci(number-2)
 
-for number in range(0,number+1):
-    print(fibonacci(number))
+def main():
+    print("HI")
+    parser = argparse.ArgumentParser(description='This program finds the nth number in the Fibonacci sequence and outputs to the file directed.')
 
-try:
-    f = open(filename, "w")
+    parser.add_argument('-n', dest='number', type=int, help="Sequence number in Fibonacci sequence")
+    parser.add_argument('-f', metavar='filename', type=str, nargs="+", help="String for file name")
 
-    for number in range(0,number+1):
-        f.write(str(fibonacci(number)) + "\n")
+    args = parser.parse_args()
+    number = args.number
+    print(args.number)
+    print(args.filename)
 
-except:
-    print("Error")
+    #filename=input("What is the name of the file: ")
+    #number=int(input("How many terms: "))
 
-f.close()
+
+
+    for n in range(0,number+1):
+        print(fibonacci(n))
+
+    try:
+        f = open(args.filename, "w")
+
+        for number in range(0,number+1):
+            f.write(str(fibonacci(number)) + "\n")
+
+    except:
+        print("Error")
+
+    f.close()
+if __name__ == '__main__':
+    main()
