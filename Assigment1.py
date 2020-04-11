355e7f9e1d940af344527d3084d4e9420c6d99b4
CallieR23 <62801357+CallieR23@users.noreply.github.com>
2020-04-11 13:59:27 -0400

    Update1 Assignment1.py

--- Assigment1.py
+++ Assigment1.py
@@ -4,14 +4,12 @@ import argparse
 
 parser = argparse.ArgumentParser(description='This program finds the nth number in the Fibonacci sequence and outputs to the file directed.')
 
-parser.add_argument('-n', metavar='number', type=str, nargs="+", help="Sequence number in Fibonacci sequence")
+parser.add_argument('-n', metavar='number', dest='number', type=int, nargs="+", help="Sequence number in Fibonacci sequence")
 parser.add_argument('-f', metavar='filename', type=str, nargs="+", help="String for file name")
 
 args = parser.parse_args()
-
-number = args.s[0]
-
-print(args.s[0])
+number = args.number
+print(args.number)
 
 filename=input("What is the name of the file: ")
 number=input("How many terms: ")
@@ -24,14 +22,14 @@ def fibonacci(number):
     else:
         return fibonacci(number-1) + fibonacci(number-2)
 
-for number in range(0,number):
+for number in range(0,number+1):
     print(fibonacci(number))
 
 try:
     f = open(filename, "w")
 
-    for number in range(0,number):
-        f.append(fibonacci(number))
+    for number in range(0,number+1):
+        f.write(str(fibonacci(number)) + "\n")
 
 except:
     print("Error")
